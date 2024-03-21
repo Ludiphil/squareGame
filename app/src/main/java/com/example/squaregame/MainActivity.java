ae1dd9d9e20d331320e07c634abe0ffad320557b
HageshiiYagi <figaro.ains@gmail.com>
2024-03-21 11:36:10 +0100

    Changement nom des images Ajout d'un bouton qui change de couleur OnClick

--- app/src/main/java/com/example/squaregame/MainActivity.java
+++ app/src/main/java/com/example/squaregame/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.squaregame;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +12,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.squaregame.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
     private ActivityMainBinding binding;
+    Integer Value;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,4 +26,22 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.Button1.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        int ID = view.getId();
+        if(ID == binding.Button1.getId()) {
+            if ((Value%2)==0){
+                binding.Button1.setImageResource(R.drawable.rouge);
+                Value = Value+1;
+            }
+
+        }
+    }
+
+
 }
