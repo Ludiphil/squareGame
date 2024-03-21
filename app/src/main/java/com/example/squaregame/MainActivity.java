438e163503609bb8693f48a18a313e09053a214f
Ludiphil <jocelyn.grelier@reseau.eseo.fr>
2024-03-21 11:40:55 +0100

    poubelle

--- app/src/main/java/com/example/squaregame/MainActivity.java
+++ app/src/main/java/com/example/squaregame/MainActivity.java
@@ -15,10 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Trait[] board = new Trait[49];
+        for (int i = 0; i < 49; i++) {
+            Trait trait = new Trait(i);
+            board[i] = trait;
+            System.out.println(board[i].getId());
+        }
     }
 }
---
+++ app/src/main/java/com/example/squaregame/Trait.java
@@ -0,0 +1,16 @@
+package com.example.squaregame;
+
+public class Trait {
+    private int id;
+
+    public Trait(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
